6698f2629fead9c411c51b28873a1d00aebc6e37
Username070 <tarnas47@gmail.com>
2022-03-27 00:22:38 +0200

    change class to className

--- client/src/components/Spinner.jsx
+++ client/src/components/Spinner.jsx
@@ -1,8 +1,8 @@
 const Spinner = () => {
 
   return (
-    <div class="position-absolute d-flex flex-column align-items-center justify-content-center" id="spinner-container">
-      <div class="spinner-border" role="status">
+    <div className="position-absolute d-flex flex-column align-items-center justify-content-center" id="spinner-container">
+      <div className="spinner-border" role="status">
       </div>
     </div>
   )
