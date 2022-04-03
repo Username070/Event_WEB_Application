950b4f41bfeacbac626c6e71096560d5422a5a42
Username070 <tarnas47@gmail.com>
2022-04-04 02:56:25 +0300

    Showing date and time of event

--- client/src/components/EventCard.jsx
+++ client/src/components/EventCard.jsx
@@ -1,6 +1,7 @@
-const EventCard = (props) => {
+import { FaCalendarAlt } from "react-icons/fa";
 
-    const { image, title, description } = props.data
+const EventCard = (props) => {
+    const { image, title, description, date, time } = props.data
     const addStr = (str, index, stringToAdd) => {
         return str.substring(0, index) + stringToAdd + str.substring(index, str.length);
     }
@@ -15,6 +16,14 @@ const EventCard = (props) => {
                         <h5 className="card-title">{title}</h5>
                         <p className="card-text event-truncate">{description}</p>
                         <button type="button" className="btn btn-primary float-end">More..</button>
+                    </div>
+                    <div class="card-footer">
+                        <small class="text-muted">
+                            <span className="d-flex align-items-center">
+                                <FaCalendarAlt className="me-2"/>
+                                {date.split("T")[0]}, {time}
+                            </span>
+                        </small>
                     </div>
                 </div>
             </div>
