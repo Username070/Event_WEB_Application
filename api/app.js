141194f437ef7f86e70a9fa2261f0a1f679958ac
Username070 <tarnas47@gmail.com>
2022-04-11 19:56:18 +0300

    typo in api/app.js

--- api/app.js
+++ api/app.js
@@ -30,7 +30,7 @@ app.use('/api/users', require('./routes/userRoutes'));
 app.use('/api/events', require('./routes/eventRoutes'));
 app.use(errorHandler)
 
-if(process_env.NODE_ENV === "production") {
+if(process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "../client/build")));
   app.get("*", (req, res) => res.sendFile(
     path.resolve(__dirname, "../", "client", "build", "index.html")
